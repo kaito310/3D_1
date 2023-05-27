e3887f003497c1f0b9a4e82f8e1ea5b7916cb3d0
kaito310 <7415kaito.s@gmail.com>
2023-05-27 16:27:26 +0900

    CameraControllerを追加(未完)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    GameObject player;
     Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position;
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        
+        transform.position += offset;
     }
 }
